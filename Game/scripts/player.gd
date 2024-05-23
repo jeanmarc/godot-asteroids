62197525151831bb73ee7d5caeadf3f23665deb1
Jean-Marc van Leerdam <jmvanleerdam@gmail.com>
2024-05-23 17:46:18 +0200

    Rotate ship

--- Game/scripts/player.gd
+++ Game/scripts/player.gd
@@ -1,12 +1,22 @@
 extends CharacterBody2D
 
 @export var acceleration := 10.0
-@export var maxSpeed := 400
+@export var max_speed := 400.0
+@export var rotation_speed := 4.0
 
 func _physics_process(delta):
 	var input_vector := Vector2(0, Input.get_axis("forward", "back"))
 	velocity += input_vector * acceleration
-	velocity = velocity.limit_length(maxSpeed)
+	velocity = velocity.limit_length(max_speed)
+
+	if Input.is_action_pressed("rotate_cw"):
+		rotate(delta * rotation_speed)
+
+	if Input.is_action_pressed("rotate_ccw"):
+		rotate(-1.0 * delta * rotation_speed)
+
+	if input_vector.y == 0:
+		velocity = velocity.move_toward(Vector2.ZERO, 2)
 
 	move_and_slide()
 
